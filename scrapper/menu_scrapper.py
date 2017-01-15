90742c78507a0b23037c9dd9ae0c4177668d2031
sbrand83 <stefan.brand@policystat.com>
2017-01-15 01:13:23 -0500

    can scrape meal data into JSON

--- scrapper/menu_scrapper.py
+++ scrapper/menu_scrapper.py
@@ -1,6 +1,46 @@
 from bs4 import BeautifulSoup
 import requests
+import dryscrape
+import json
+from Item import Item
 
-menu_page = requests.get("http://rose-hulman.cafebonappetit.com/");
-soup = BeautifulSoup(menu_page.content, 'html.parser')
-print(soup.prettify())
+url = "http://rose-hulman.cafebonappetit.com/"
+
+session = dryscrape.Session()
+
+print("Retrieving page...")
+session.visit(url)
+
+
+print("Scraping meal information...")
+menu_page = session.body()
+soup = BeautifulSoup(menu_page, 'html.parser')
+
+breakfast = soup.select("#panel-daypart-menu-1 article .daypart-menu .column article")
+lunch = soup.select("#panel-daypart-menu-2 article .daypart-menu .column article")
+dinner = soup.select("#panel-daypart-menu-3 article .daypart-menu .column article")
+
+meals = []
+
+for meal in [breakfast, lunch, dinner]:
+    meal_dict = {"hours": "", "items": []}
+    for item in meal:
+        item = BeautifulSoup(str(item), 'html.parser')
+        item_name = item.span
+        item_icons = item.select(".station-item-title .cor-icons")
+
+        icons = BeautifulSoup(str(item_icons), 'html.parser')
+        icon_titles = list(img.get('title') for img in icons.find_all('img'))
+
+        calories = item.select('ul .nutrition span')
+        calories = BeautifulSoup(str(calories), 'html.parser')
+        calories = calories.get_text()
+
+        item_name = item_name.get_text().strip()
+        item = {"name": item_name, "icons": icon_titles, "calories": calories}
+        meal_dict["items"].append(item)
+    meals.append({"meal": meal_dict})
+
+print(json.dumps(meals))
+
+
