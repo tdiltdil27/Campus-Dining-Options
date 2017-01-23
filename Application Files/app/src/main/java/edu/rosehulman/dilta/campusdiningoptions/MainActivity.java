b52165362742c60f20bd2652deaa4e90be23d046
tdiltdil27 <dilta@rose-hulman.edu>
2017-01-23 01:39:42 -0500

    Calendar returning date and day of week

--- Application Files/app/src/main/java/edu/rosehulman/dilta/campusdiningoptions/MainActivity.java
+++ Application Files/app/src/main/java/edu/rosehulman/dilta/campusdiningoptions/MainActivity.java
@@ -1,6 +1,8 @@
 package edu.rosehulman.dilta.campusdiningoptions;
 
+import android.app.DatePickerDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -9,25 +11,47 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.CalendarView;
+import android.widget.DatePicker;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.GregorianCalendar;
+
+import static android.content.DialogInterface.BUTTON_NEGATIVE;
+import static android.content.DialogInterface.BUTTON_POSITIVE;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String CALENDAR_DIALOG_TITLE = "Choose a date";
     private String currentDate;
+    private String focusedDate;
     private int numPages = 2;
     private SectionsPagerAdapter mSectionsPagerAdapter;
     private ViewPager mViewPager;
     private MealTimeAdapter mAdapter;
+
+    private DatePickerDialog datePicker;
+
+    private int mCurrentYear;
+    private int mCurrentMonth;
+    private int mCurrentDay;
+
+    private int mYear;
+    private int mMonth;
+    private int mDay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,22 +60,10 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        Calendar calendar = GregorianCalendar.getInstance();
-        currentDate = "Today, " + (calendar.get(Calendar.MONTH)+1) + "/" + calendar.get(Calendar.DAY_OF_MONTH)+"/"+ calendar.get(Calendar.YEAR);
-        setTitle(currentDate);
-
-
+        setDate();
+        updateTitle();
 
         mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
-
-//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
 
         // Set up the ViewPager with the sections adapter.
         mViewPager = (ViewPager) findViewById(R.id.container);
@@ -60,6 +72,20 @@ public class MainActivity extends AppCompatActivity {
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(mViewPager);
 
+    }
+
+    private void setDate() {
+        Calendar calendar = GregorianCalendar.getInstance();
+        mCurrentMonth = calendar.get(Calendar.MONTH)+1;
+        mCurrentDay = calendar.get(Calendar.DAY_OF_MONTH);
+        mCurrentYear = calendar.get(Calendar.YEAR);
+
+        mMonth = calendar.get(Calendar.MONTH)+1;
+        mDay = calendar.get(Calendar.DAY_OF_MONTH);
+        mYear = calendar.get(Calendar.YEAR);
+
+        currentDate = "Today, " + (mCurrentMonth) + "/" + mCurrentDay +"/"+ mCurrentYear;
+        focusedDate = "Today, " + (mMonth) + "/" + mDay +"/"+ mYear;
     }
 
     @Override
@@ -76,12 +102,58 @@ public class MainActivity extends AppCompatActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-//        if (id == R.id.action_settings) {
-//            return true;
-//        }
+        if (id == R.id.action_calendar) {
+
+            createCalendarDialog();
+            return true;
+        }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void createCalendarDialog() {
+        datePicker = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                mYear = year;
+                mMonth = monthOfYear+1;
+                mDay = dayOfMonth;
+            }
+        }, mYear, mMonth-1, mDay );
+//        builder.setTitle(CALENDAR_DIALOG_TITLE);
+
+        datePicker.setButton(BUTTON_POSITIVE, "Ok", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                mDay = datePicker.getDatePicker().getDayOfMonth();
+                mYear = datePicker.getDatePicker().getYear();
+                mMonth = datePicker.getDatePicker().getMonth()+1;
+
+                updateTitle();
+            }
+        });
+
+        datePicker.setButton(BUTTON_NEGATIVE, "Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                // Do nothing.
+            }
+        });
+        datePicker.show();
+    }
+
+    private void updateTitle() {
+        focusedDate = "Today, " + (mMonth) + "/" + mDay +"/"+ mYear;
+
+        SimpleDateFormat sdf = new SimpleDateFormat("EEEE");
+        Calendar cal = GregorianCalendar.getInstance();
+        cal.set(mYear-1900, mMonth, mDay);
+        String dayOfTheWeek = sdf.format(cal.getTime());
+
+        if(focusedDate.equals(currentDate)) {
+
+        } else {
+            focusedDate = dayOfTheWeek + ", " + (mMonth) + "/" + mDay +"/"+ mYear;
+        }
+        setTitle(focusedDate);
     }
 
 
@@ -120,9 +192,9 @@ public class MainActivity extends AppCompatActivity {
         public CharSequence getPageTitle(int position) {
 
             if(position == 0) {
-                return "Cafe Meal Times";
+                return "Cafe Menu";
             } else if (position == 1) {
-                return "All Hours";
+                return "Location Hours";
             } else {
                 return null;
             }
